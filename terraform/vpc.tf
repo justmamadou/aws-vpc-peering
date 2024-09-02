ca13fb736638f44be37f214371b24996da215ba4
iam_mamadou <momodouba44@gmail.com>
2024-09-02 23:52:42 +0000

    Fea:  Terraform VPC Module

---
+++ terraform/vpc.tf
@@ -0,0 +1,39 @@
+module "vpc_1" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc-1"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+    VPC = "VPC-1"
+  }
+}
+
+module "vpc_2" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc-1"
+  cidr = "10.1.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+    VPC = "VPC-1"
+  }
+}
